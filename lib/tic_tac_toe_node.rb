3a81a3bc64ef02676b7169422f5ce8c5ef546202
Eduardo Gonzalez <edugonzavila@gmail.com>
2022-03-14 20:41:39 -0700

    Initial commit; Generate children; Identify if a node is a #losing_node?

---
+++ lib/tic_tac_toe_node.rb
@@ -0,0 +1,51 @@
+require_relative 'tic_tac_toe'
+require "byebug"
+
+class TicTacToeNode
+  NEXT_MARK = { x: :o, o: :x}
+
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @opponent = NEXT_MARK[@next_mover_mark]
+    @prev_move_pos = prev_move_pos
+  end
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos#, :children
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  private_constant :NEXT_MARK
+  def children
+    children_nodes = []
+    @board.rows.each.with_index do |row, ri|
+      next if row.none?(&:nil?)
+      row.each.with_index do |col, ci|
+        next unless col.nil?
+        dupboard = @board.dup
+        pos = [ri, ci]
+        dupboard[pos] = @next_mover_mark
+        new_node = TicTacToeNode.new(dupboard, @opponent, pos)
+        children_nodes << new_node
+      end
+    end
+    children_nodes
+  end
+
+  def losing_node?(evaluator)
+    if @board.over?
+      return true if @board.winner == NEXT_MARK[evaluator]
+      return false if @board.winner.nil? || @board.winner == evaluator
+    end
+
+    self.children.all? { |child| child.losing_node?(evaluator) } ||
+    self.children.any? { |child| child.losing_node? (evaluator) }
+  end
+
+end
