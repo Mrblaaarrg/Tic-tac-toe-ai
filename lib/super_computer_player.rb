e8dd0791676d5bfeaed2499fa0a6fa20d1d1689b
Eduardo Gonzalez <edugonzavila@gmail.com>
2022-03-14 22:04:43 -0700

    Add superAI move

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -1,7 +1,21 @@
 require_relative 'tic_tac_toe_node'
 
+
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    # node.children.each { |child| return child.prev_move_pos if child.winning_node?(mark) }
+    # node.children
+    #   .reject { |child| child.losing_node?(mark) }
+    #   .first
+    #   .prev_move_pos
+
+    possible_moves = node.children.shuffle
+    target_node = possible_moves.find { |child| child.winning_node?(mark) }
+    return target_node.prev_move_pos if target_node
+    target_node = possible_moves.find { |child| !child.losing_node?(mark) }
+    return target_node.prev_move_pos if target_node
+    raise "Error, this is impossible, I am PERFECT!"
   end
 end
 
