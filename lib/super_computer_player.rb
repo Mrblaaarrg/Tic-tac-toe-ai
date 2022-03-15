0a487a9f025b237751b9ab2f97b9279da0d78e92
Eduardo Gonzalez <edugonzavila@gmail.com>
2022-03-15 10:15:05 -0700

    Add error when computer cannot win

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -4,18 +4,19 @@ require_relative 'tic_tac_toe_node'
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
     node = TicTacToeNode.new(game.board, mark)
-    node.children.each { |child| return child.prev_move_pos if child.winning_node?(mark) }
-    node.children
+    children = node.children
+    children.each { |child| return child.prev_move_pos if child.winning_node?(mark) }
+    non_losing = children
       .reject { |child| child.losing_node?(mark) }
       .first
-      .prev_move_pos
+    return non_losing.prev_move_pos if non_losing
 
     # possible_moves = node.children.shuffle
     # target_node = possible_moves.find { |child| child.winning_node?(mark) }
     # return target_node.prev_move_pos if target_node
     # target_node = possible_moves.find { |child| !child.losing_node?(mark) }
     # return target_node.prev_move_pos if target_node
-    # raise "Error, this is impossible, I am PERFECT!"
+    raise "Error, this is impossible, I am PERFECT!"
   end
 end
 
